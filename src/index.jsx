6133df1778842fcf89b46ba2bc65df549a9f832c
ida iannone <ida.ian.13@gmail.com>
2023-02-27 12:16:58 +0100

    Co-authored-by: Giuseppe-Df <Giuseppe-Df@users.noreply.github.com>

---
+++ src/index.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import App from './App';
+import ReactDOM from 'react-dom/client';
+import 'mdb-react-ui-kit/dist/css/mdb.min.css';
+import "@fortawesome/fontawesome-free/css/all.min.css";
+
+<link href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap" rel="stylesheet" />
+
+const root = ReactDOM.createRoot(document.getElementById('container')).render(
+  <React.StrictMode>
+    <App/>
+  </React.StrictMode>
+);
+
